a7bde97a383fe7d97cb0c9e6b71973ba42eb3552
takeshi <tkishita.appqualia@gmail.com>
2018-07-29 14:32:14 +0900

    fix: シェルからのコマンドをROSパッケージの起動だけに修正

--- joy_control/launch/run_joy_control.sh
+++ joy_control/launch/run_joy_control.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   echo "指定された引数がありません。" 1>&2
-  echo "実行するには2個の引数が必要です。" 1>&2
-  echo "1: ROS_IP" 1>&2
-  echo "2: ROS_MASTER_URI" 1>&2
+  echo "実行するには1個の引数が必要です。" 1>&2
+  echo "1: ROS_IP（端末のIPアドレス）" 1>&2
+
   exit 1
 fi
 
 # 自身IPの指定
 export ROS_IP=$1
-# master側のIPを指定
-export ROS_MASTER_URI=http://$2:11311
-# コントロールスクリプトの実行
-rosrun joy_control joy_test.py &
+roslaunch joy_control joy_control.launch
