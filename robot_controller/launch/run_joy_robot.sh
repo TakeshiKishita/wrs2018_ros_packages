41e6f32ffdecdb1dafadbf81622dc6e9fd542ee0
takeshi <xxtkcxx@gmail..com>
2018-09-16 18:35:02 +0900

    feat: launchファイルとシェルの追加

---
+++ robot_controller/launch/run_joy_robot.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]; then
+  echo "指定された引数がありません。" 1>&2
+  echo "実行するには2個の引数が必要です。" 1>&2
+  echo "1: ROS_IP（端末のIPアドレス）" 1>&2
+  echo "2: ROS_MASTER_URI（roscoreを起動している端末のIPアドレス）" 1>&2
+  exit 1
+fi
+
+# 自身IPの指定
+export ROS_IP=$1
+# master側のIPを指定
+export ROS_MASTER_URI=http://$2:11311
+# コントロールスクリプトの実行
+rosrun robot_control joy_controll.py
