bfb3d58a34863702c9de6643ccc5c45297f76fc9
TKC <xxtkcxx@gmail.com>
2018-10-07 16:40:58 +0900

    fix: 接続が途切れても落ちないよう修正

--- robot_controller/scripts/arm_angle_send.py
+++ robot_controller/scripts/arm_angle_send.py
@@ -11,11 +11,16 @@ rospy.init_node('arm_controller')
 
 pub = rospy.Publisher('arm_controller_angle', String, queue_size=10)
 rate = rospy.Rate(10)
-with serial.Serial('/dev/ttyUSB0', 9600) as ser:
-    while not rospy.is_shutdown():
-        angle_list_str = ser.readline()
-        pub.publish(angle_list_str)
-        rate.sleep()
-        rospy.loginfo(angle_list_str[:-2])
-ser.close()
+while True:
+    try:
+        with serial.Serial('/dev/ttyUSB0', 9600) as ser:
+            while not rospy.is_shutdown():
+                angle_list_str = ser.readline()
+                pub.publish(angle_list_str)
+                rate.sleep()
+                rospy.loginfo(angle_list_str[:-2])
 
+            ser.close()
+            break
+    except:
+        continue
